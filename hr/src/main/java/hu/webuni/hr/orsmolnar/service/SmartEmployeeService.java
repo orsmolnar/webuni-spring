2238817316722805625941dc2bbb6e8c72d14f43
orsmo <orsmo@HP>
2021-10-12 12:45:40 +0200

    first week final

--- hr/src/main/java/hu/webuni/hr/orsmolnar/service/SmartEmployeeService.java
+++ hr/src/main/java/hu/webuni/hr/orsmolnar/service/SmartEmployeeService.java
@@ -1,17 +1,12 @@
 package hu.webuni.hr.orsmolnar.service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.Period;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 import hu.webuni.hr.orsmolnar.config.HrConfigProperties;
@@ -38,7 +33,6 @@ public class SmartEmployeeService implements EmployeeService{
 		List<Range> sortedRanges = ranges.stream()
 											.sorted(compareByTerm.reversed())
 											.collect(Collectors.toList());
-		
 		
 		for (Range range : sortedRanges ) {
 			if (employeeNumOfMonths >= (range.getYearsOfTerm()*12 + range.getMonthsOfTerm())) {
