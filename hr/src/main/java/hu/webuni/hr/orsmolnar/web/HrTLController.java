b534cc0423f600e9977cecb89e6a47e07de540e0
orsmo <orsmo@HP>
2021-10-21 22:12:18 +0200

    TLController extended (editEmployee, deleteEmployee)

--- hr/src/main/java/hu/webuni/hr/orsmolnar/web/HrTLController.java
+++ hr/src/main/java/hu/webuni/hr/orsmolnar/web/HrTLController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import hu.webuni.hr.orsmolnar.model.Employee;
@@ -41,4 +42,31 @@ public class HrTLController {
 		return "redirect:employees";
 	}
 	
+	@GetMapping("/deleteEmployee/{id}")
+	public String deleteEmployee(@PathVariable long id) {
+		allEmployees.removeIf(emp -> emp.getId().equals(id));
+		return "redirect:/employees";
+	}
+	
+	// alkalmazott módosítása 2 lépésben törtrénik: 1.link átvisz egy editáló oldalra (GET), 2.beküldöm a szerkesztett form-ot (POST)
+	@GetMapping("/employees/{id}")
+	public String editEmployee(@PathVariable long id, Map<String, Object> model) {
+		model.put("employee", allEmployees.stream()
+										  .filter(emp -> emp.getId().equals(id))
+										  .findFirst()
+										  .get());
+		return "editEmployee";
+	}
+	
+	@PostMapping("/updateEmployee")
+	public String updateEmployee(Employee employee) {
+		
+		for(int i=0; i < allEmployees.size(); i++) {
+			if (allEmployees.get(i).getId().equals(employee.getId())) {
+				allEmployees.set(i, employee);
+				break;
+			}
+		}
+		return "redirect:employees";
+	}
 }
